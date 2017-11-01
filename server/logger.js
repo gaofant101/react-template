6829522b30d96d59fca5588e418b155e43b48d5c
gao <evanhunt.g@gmail.com>
2017-11-01 11:02:44 +0800

    modify config

--- server/logger.js
+++ server/logger.js
@@ -24,7 +24,11 @@ const logger = {
             console.log(`Tunnel initialised ${chalk.green('✓')}`);
         }
 
-        console.log(`${chalk.bold('Access URLs:')}${divider} Localhost: ${chalk.magenta(`http://${host}:${port}`)} LAN: ${chalk.magenta(`http://${ip.address()}:${port}`) + (tunnelStarted ? `\n    Proxy: ${chalk.magenta(tunnelStarted)}` : '')}${divider} ${chalk.blue(`Press ${chalk.italic('CTRL-C')} to stop`)} `);
+        console.log(`${chalk.bold('Access URLs:')}${divider}
+        Localhost: ${chalk.magenta(`http://${host}:${port}`)}
+        LAN: ${chalk.magenta(`http://${ip.address()}:${port}`) + (tunnelStarted ? `\n
+        Proxy: ${chalk.magenta(tunnelStarted)}` : '')}${divider}
+        ${chalk.blue(`Press ${chalk.italic('CTRL-C')} to stop`)} `);
     },
 };
 
