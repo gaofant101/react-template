5a64bb85d6bfb4220ee080abaadb3a522190cbf4
fan@@@ <evanhunt.g@gmail.com>
2018-10-26 10:40:47 +0800

    optimization large file split chunks configuration

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -4,13 +4,9 @@
 const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-// Remove this line once the following warning goes away (it was meant for webpack loader authors not users):
-// 'DeprecationWarning: loaderUtils.parseQuery() received a non-string value which can be problematic,
-// see https://github.com/webpack/loader-utils/issues/56 parseQuery() will be replaced with getOptions()
-// in the next major version of loader-utils.'
-process.noDeprecation = true;
 const devMode = process.env.NODE_ENV !== 'production';
+
+process.noDeprecation = true;
 
 module.exports = (options) => ({
     mode: options.mode,
@@ -135,14 +131,6 @@ module.exports = (options) => ({
         ],
     },
     plugins: options.plugins.concat([
-        new webpack.ProvidePlugin({
-            // make fetch available
-            fetch: 'exports-loader?self.fetch!whatwg-fetch',
-        }),
-
-        // Always expose NODE_ENV to webpack, in order to use `process.env.NODE_ENV`
-        // inside your code for any environment checks; UglifyJS will automatically
-        // drop any unreachable code.
         new webpack.DefinePlugin({
             'process.env': {
                 NODE_ENV: JSON.stringify(process.env.NODE_ENV),
